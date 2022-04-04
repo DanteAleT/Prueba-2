3d8742d836ebcc5ac5fc1046ea5fa25d4078d979
DanteAle <danteale01@hotmail.com>
2022-04-04 15:06:53 -0300

    tercer commit

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-let numero = 3;
+let plataCofla = prompt("cuanto dinero tenes?");
 
-numero **= 3;
+plataCofla = parseInt(plataCofla);
 
-alert(numero);
+//palito helado : 0.6 
+//palito helado de crema: 1
+//bombon helado marca heladix: 1.6
+//bombon helado marca heladovich: 1.7
+//bombon helado marca helardo: 1.8
+//potencito de helado con confites: 2.9
+//pote de 1/4 Kg: 2.9
+
+
+if (plataCofla >= 0.6 && plataCofla < 1){ alert(
+"comprate el helado mas barato") 
+}
+else if(plataCofla >= 1 && plataCofla < 1.6){  alert(
+"comprate el helado de crema") }
+else if(plataCofla >= 1.6 && plataCofla < 1.7){ alert(  
+"comprate el helado de heladix") }
+else if(plataCofla >= 1.7 && plataCofla < 1.8){ alert(  
+"comprate el helado de heladovich") }
+else if(plataCofla >= 1.8 && plataCofla < 2.9){ alert( 
+"comprate el helado de helardo") }
+else if(plataCofla >= 2.9){  alert(
+"comprate el helado de confite o 1/4kg") }
+else if (plataCofla < 0.6){alert(
+"no te alcanza la plata")    
+}
+else{alert("no es un valor valido")};     
